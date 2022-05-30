e821f57fc0a983420dd99ed18390ff974ba2ab66
Fonghang <30502871+hysbeta@users.noreply.github.com>
2022-05-30 12:20:50 +0800

    Update jdlite_15_8.py

--- jdlite_15_8.py
+++ jdlite_15_8.py
@@ -75,11 +75,12 @@ def qiang_quan(cookie, i, index):
         res = requests.post(url=url, headers=headers, data=data).json()
         # print(res)
         if res['code'] == '0':
-            print(f"账号{index + 1}：{res['subCodeMsg']}")
+            #print(f"账号{index + 1}：{res['subCodeMsg']}")
             if '成功' in res['subCodeMsg']:
                 content.append(f"账号{cookie[90:-1]}：{res['subCodeMsg']}")
         else:
-            print(f"账号{index + 1}：{res['errmsg']}")
+            pass
+            #print(f"账号{index + 1}：{res['errmsg']}")
     except:
         pass
 
